d259d21ec6c9e431f846edc6554c349d71a48726
Shreshtth Kumar Agarwaal <agarwalshreshth3@gmail.com>
2024-11-27 01:04:11 +0530

    added chat history

--- rag_server/app.py
+++ rag_server/app.py
@@ -48,6 +48,9 @@ from langchain_pinecone import PineconeVectorStore
 
 app = Flask(__name__)
 CORS(app)
+
+chat_history=[]
+
 @app.route("/", methods = ['GET'])
 def server_status():
     return jsonify({"status": "server running"})
@@ -59,6 +62,14 @@ def answer():
     index_name = "rag-huggingface"
     query = data.get('query')
     print(query)
+    
+    history_context = "\n".join([f"{entry['role']}: {entry['content']}" for entry in chat_history])
+    if history_context:
+        query_with_context = f"Conversation so far:\n{history_context}\n\nUser: {query}\nAssistant:"
+    else:
+        query_with_context = query
+
+    
     llm=ChatGroq(
         groq_api_key=os.environ["GROQ_API_KEY"],
         model_name="llama-3.1-8b-instant",
@@ -70,6 +81,15 @@ def answer():
         chain_type="stuff",
         retriever=knowledge.as_retriever()
     )
-    return jsonify({"answer": qa.invoke(query).get("result")})
+    answer = qa.run(query_with_context)
+    chat_history.append({"role": "user", "content": query})
+    chat_history.append({"role": "assistant", "content": answer})
+    response = {
+        "answer": answer,
+        "chat_history": chat_history
+    }
+    
+    return jsonify(response)
+    # return jsonify({"answer": qa.invoke(query).get("result")})
 if __name__ == "__main__":
-    app.run(debug=True , port = 5001)   
+    app.run(debug=True , port = 5001)
